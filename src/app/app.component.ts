1ee18523c349a99b73f1000e5ba249377c2d76d9
Fehim Mahfoud <fehim.mahfoud19@gmail.com>
2019-09-29 01:07:00 +0100

    Affichage d'un contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ContactWeb';
+  contact : {
+    "nom" : "Persone",
+    "email" : "email@gmail.com"
+  }
 }
