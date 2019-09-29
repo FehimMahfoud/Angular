398ae29a4744f3ef5580f91d63979f61515e9702
Fehim Mahfoud <fehim.mahfoud19@gmail.com>
2019-09-29 22:55:51 +0100

    Route vers about et contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes} from "@angular/router"
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { from } from 'rxjs';
+import { AboutComponent } from './about/about.component';
+
+const appRoute:Routes= [
+  { 
+    path:"about"
+   ,component:AboutComponent
+  },
+  {
+    path:"contacts",
+    component:ContactsComponent
+  }
+
+]
+
 
 @NgModule({
   declarations: [
