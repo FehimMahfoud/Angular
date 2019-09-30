3d241b36c5a910dc381149b950cc4802e0c9c1a3
Fehim Mahfoud <fehim.mahfoud19@gmail.com>
2019-09-30 14:46:12 +0100

    Fix probleme

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http"
-
-import { from } from 'rxjs';
-
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-about',
@@ -10,10 +8,13 @@ import { from } from 'rxjs';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  pageContacts :any;
   constructor(private http:HttpClient) { }
 
   ngOnInit() {
-    this.http.get('http://localhost:8080/chercheContacts?mc=FEHIM&size=5&page=0');
+    this.http.get('http://localhost:8080/chercheContacts?mc=FEHIM&size=5&page=0').pipe(map(resp=>resp.toString())).subscribe(data=>{
+      this.pageContacts = data;
+    });  
   }
 
 }
